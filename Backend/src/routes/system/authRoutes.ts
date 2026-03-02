b22c192e8f1164088d1c4b4161f844f3d2024cc6
Victor Neves <victoneves03@hotmail.com>
2026-03-02 20:41:14 -0300

    Limpando bakend para receber novo modelo e estrutura usando nestjs

--- Backend/src/routes/system/authRoutes.ts
+++
@@ -1,8 +0,0 @@
-// import express from 'express';
-
-// export default (router: express.Router) => {
-//   router.post('/auth/register');
-//   router.post('/auth/refreshToken');
-//   router.post('/auth/login');
-//   router.post('/auth/logout');
-// }
